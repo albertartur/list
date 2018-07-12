f8c487e0cf52e0429b054c280ffbfabf726de1b2
aa142536 <a480011@mail.ru>
2018-07-12 13:12:55 +0400

    change ads.php

--- application/views/category/ads.php
+++ application/views/category/ads.php
@@ -36,11 +36,11 @@
 						<img width="52px" src="<?= base_url('img/top.png'); ?>">
 						<span text-right>Տոպ</span>
 					</div>
-					<div  data-toggle="modal" data-target="#home_mod" id="home_mod" class="col-md-4">
+					<div  data-toggle="modal" data-target="#home_mod" class="col-md-4">
 						<img width="52px" src="<?= base_url('img/home.png'); ?>">
 						<span>գլխավոր էջ</span>
 					</div>
-					<div  data-toggle="modal" data-target="#free_mod" id="free_mod" class="col-md-4">
+					<div  data-toggle="modal" data-target="#free_mod" class="col-md-4">
 						<img width="52px" src="<?= base_url('img/free.png'); ?>">
 						<span>Շտապ</span>
 					</div>
@@ -150,6 +150,76 @@
       	<div class="text-center">
       		<p><a href="">Մանրամասներ >></a></p>      		
       	</div>
+
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Modal Home-->
+<div id="home_mod" class="modal fade" role="dialog">
+  <div class="modal-dialog ">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Տեղադրել գլխավոր էջի վրա
+
+</h4>
+      </div>
+      <p>Հայտարարությունը կտեղադրվի կայքի գլխավոր էջի վրա` այլ հայտարարությունների հետ պարբերական կրկնության սկզբունքով:</p>
+      <div class="border"></div>
+      <div class="modal-body ">
+        <ol>
+          <li>Առավելագույն քանակությամբ դիտումներ` կայքի ամենաբարձր այցելություն ունեցող էջում:</li>
+          <li>Առաջինը, ինչ կտեսնեն կայքի այցելուները, դա գլխավոր էջում տեղադրված Ձեր հայտարարությունն է:</li>
+          <li>Հայտարարությունը առանձնացված կլինի դեղին գույնով մինչ ժամկետի սպառվելը:</li>
+        </ol>
+        <div class="border"></div>
+        <div class="text-center">
+          <p><a href="">Մանրամասներ >></a></p>          
+        </div>
+
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Modal free-->
+<div id="free_mod" class="modal fade" role="dialog">
+  <div class="modal-dialog ">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Դարձնել շտապ</h4>
+      </div>
+      <p>Հայտարարությունների ցանկում և որոնման արդյունքում Ձեր հայտարարութունը նշված կլինի Շտապ! նշանով:</p>
+      <div class="border"></div>
+      <div class="modal-body ">
+        <ol>
+          <li>"Շտապ!" նշանը պահպանվում է մինչև Ձեր հայտարարության ժամկետի սպառվելը:</li>
+          <li>"Շտապ!" հայտարարությունները կայքի այցելուներից ստանում են ավելի շատ արձագանքներ:</li>
+          <li>Իդեալական հնարավորություն է գրավել ավելի մեծ քանակով այցելուների ուշադրությունը Ձեր հայտարարության նկատմամբ:</li>
+        </ol>
+        <div class="border"></div>
+        <div class="text-center">
+          <p><a href="">Մանրամասներ >></a></p>          
+        </div>
 
 
 
